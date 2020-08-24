c2ef41084b5f333aec586b4f35607d6e94322d67
lukasss88 <lukaszszymanski88.99@gmail.com>
2020-08-24 14:50:03 +0200

    blalbal

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
-(function(){
-    'use strict';
+(function () {
+  'use strict';
 
-    angular.module('app', ['ngSanitize']);
-
+  angular.module('app', ['ngSanitize']);
 })();
--- app/main.controller.js
+++ app/main.controller.js
@@ -1,12 +1,11 @@
-(function(){
-    'use strict';
+(function () {
+  'use strict';
 
-    function MainController(){
-        var ctrl =  this;
+  function MainController() {
+    var ctrl = this;
+    console.log('abc');
+    ctrl.arrayValues = [14.783, 2.346, 89.2931, 16.501, 667.178];
+  }
 
-        ctrl.arrayValues = [14.783, 2.346, 89.2931, 16.501, 667.178];
-    }
-
-    angular.module('app').controller('MainController', [MainController])
-
+  angular.module('app').controller('MainController', [MainController]);
 })();
